9e8ba749de1a49df67a03fd5412f7294aa347fe7
Kevin Sage <ksagedeveloper@gmail.com>
2019-10-03 20:27:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deleteFromObjectByKey(object, key) {
   delete object[key];
   return object;
 }
-// function destructivelyDeleteFromObjectByKey(object, key) {
-//   delete object[key];
-//   return object;
-// }
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
