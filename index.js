bb4187d32c7fa00d5e3b3d8760a5a1b6369084fe
Kevin Sage <ksagedeveloper@gmail.com>
2019-10-03 20:37:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ object[key] = value;
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object, key)
   delete newObj[key];
-  return object;
+  return newObj;
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
